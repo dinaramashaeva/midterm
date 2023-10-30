1d2e3a8944f6f5fb66e06eb00305213c02534a0a
dinara <m4k4r0n999@gmail.com>
2023-10-30 17:27:07 +0600

    test

--- blog/base/views.py
+++ blog/base/views.py
@@ -3,9 +3,24 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'home.html')
+rooms = [
+    {'id': 1, 'name':'Lets learn django'},
+    {'id': 2, 'name':'Design a logo'},
+    {'id': 3, 'name':'midterm exams'},
+]
 
-def room(request):
-    return render(request, 'room.html')
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None 
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+
+    context = {'room': room}        
+
+    return render(request, 'base/room.html', context)
 
